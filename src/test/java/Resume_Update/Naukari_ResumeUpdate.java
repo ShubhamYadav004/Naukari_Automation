d990cfb4aeff15c3c2d63b0c049711917bc3af0f
Shubham Yadav <82416133+ShubhamYadav004@users.noreply.github.com>
2025-08-08 02:51:34 +0530

    Push all code

--- src/test/java/Resume_Update/Naukari_ResumeUpdate.java
+++ src/test/java/Resume_Update/Naukari_ResumeUpdate.java
@@ -1,87 +1,116 @@
 package Resume_Update;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.openqa.selenium.support.ui.WebDriverWait;
-	import org.openqa.selenium.support.ui.ExpectedConditions;
-    import org.testng.Assert;
-    import org.testng.annotations.AfterClass;
-	import org.testng.annotations.BeforeClass;
-	import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
-	public class Naukari_ResumeUpdate {
-	    private WebDriver driver;
-	    private WebDriverWait wait;
+public class Naukari_ResumeUpdate {
+	private WebDriver driver;
+	private WebDriverWait wait;
 
-	    @BeforeClass
-	    public void setUp() {
-	        driver = new ChromeDriver();
-	        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	        driver.manage().window().maximize();
-	    }
+	@BeforeClass
+	public void setUp() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-blink-features=AutomationControlled");
+		options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+		options.setExperimentalOption("useAutomationExtension", false);
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
 
-	    @Test
-	    public void testNaukriProfileUpdate() throws InterruptedException, IOException {
-	        driver.get("https://www.naukri.com/");
+		driver = new ChromeDriver(options);
 
-	        driver.findElement(By.xpath("//a[@title='Jobseeker Login']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 
-	        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(
-	            By.xpath("//input[@placeholder='Enter your active Email ID / Username']")
-	        ));
-	        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Enter your password']"));
-	        WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
-
-	        username.sendKeys("contactstoshubham@gmail.com");
-	        password.sendKeys("Rishabh@108");
-	        submit.click();
-
-	        WebElement profile = wait.until(ExpectedConditions.visibilityOfElementLocated(
-	            By.xpath("//div[@class='nI-gNb-drawer__bars']")
-	        ));
-	        profile.click();
-
-	        WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(
-	            By.xpath("//a[@class='nI-gNb-info__sub-link']")
-	        ));
-	        menu.click();
-
-	        WebElement resumeUpload = wait.until(ExpectedConditions.presenceOfElementLocated(
-	            By.xpath("//input[@type='file']")
-	        ));
-
-	        String filePath = Paths.get("C:\\Users\\shubh\\Downloads\\Shubham_Resume.pdf").toString();
-	        resumeUpload.sendKeys(filePath);
-
-	        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
-	            By.xpath("//span[@id='attachCVMsgBox']//p[@class='msg']")
-	        ));
-	        String messageText = successMessage.getText();
-	        System.out.println("Success Message: " + messageText);
-	        String expectedText = "Resume has been successfully uploaded.";
-	        Assert.assertEquals(messageText,expectedText);
-	     // Capture screenshot on success
-	        if (messageText.equals(expectedText)) {
-	            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-	            // Define the location to save the screenshot
-	            File destination = new File("C:\\Users\\shubh\\Pictures\\ResumeUploadSuccess.png");
-	            FileUtils.copyFile(screenshot, destination);
-	            System.out.println("Screenshot saved at: " + destination.getAbsolutePath());
-	        }
-	    }
-
-	    @AfterClass
-	    public void tearDown() throws InterruptedException {
-	        if (driver != null) {
-	            driver.quit();
-	        }
-	    }
+		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 	}
+
+	@Test
+	public void testNaukriProfileUpdate() throws IOException {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
+		String timestamp = LocalDateTime.now().format(formatter);
+
+		try {
+			driver.get("https://www.naukri.com/");
+
+			WebElement loginLink = wait.until(ExpectedConditions.elementToBeClickable(
+					By.xpath("//a[@title='Jobseeker Login']")
+			));
+			loginLink.click();
+
+			WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(
+					By.xpath("//input[@placeholder='Enter your active Email ID / Username']")
+			));
+			WebElement password = driver.findElement(By.xpath("//input[@placeholder='Enter your password']"));
+			WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
+
+			username.sendKeys("contactstoshubham@gmail.com");
+			password.sendKeys("Rishabh@108");
+			submit.click();
+
+			WebElement profile = wait.until(ExpectedConditions.visibilityOfElementLocated(
+					By.xpath("//div[@class='nI-gNb-drawer__bars']")
+			));
+			profile.click();
+
+			WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(
+					By.xpath("//a[@class='nI-gNb-info__sub-link']")
+			));
+			menu.click();
+
+			WebElement resumeUpload = wait.until(ExpectedConditions.presenceOfElementLocated(
+					By.xpath("//input[@type='file']")
+			));
+
+			String filePath = Paths.get("test-input", "Shubham_Resume25.pdf").toAbsolutePath().toString();
+			resumeUpload.sendKeys(filePath);
+
+			WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+					By.xpath("//span[@id='attachCVMsgBox']//p[@class='msg']")
+			));
+			String messageText = successMessage.getText();
+			System.out.println("Success Message: " + messageText);
+			String expectedText = "Resume has been successfully uploaded.";
+
+			Assert.assertEquals(messageText, expectedText);
+
+			// Screenshot on success
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File destination = new File("ResumeUploadSuccess_" + timestamp + ".png");
+			FileUtils.copyFile(screenshot, destination);
+			System.out.println("Screenshot saved at: " + destination.getAbsolutePath());
+
+		} catch (Exception e) {
+			// Screenshot on failure
+			File errorShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File dest = new File("FailureScreenshot_" + timestamp + ".png");
+			FileUtils.copyFile(errorShot, dest);
+			System.err.println("Test failed. Screenshot saved at: " + dest.getAbsolutePath());
+			throw e;
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
